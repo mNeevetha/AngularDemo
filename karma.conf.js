a4842cc50d0ce2bf60a5e267eb05e1bfc23f4a29
Neevetha <neevetha.m@cognizant.com>
2017-04-03 15:54:30 +0530

    Modified karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,6 @@
 module.exports = function(config) {
 //
+//
   var appBase    = 'app/';       // transpiled app JS and map files
   var appSrcBase = 'app/';       // app source TS files
   var appAssets  = 'app/'; // component assets fetched by Angular's compiler
