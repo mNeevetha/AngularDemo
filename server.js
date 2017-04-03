e26c9d07f954ae5d435266e2d705f0fd835f9bbb
Neevetha <neevetha.m@cognizant.com>
2017-04-03 15:41:17 +0530

    Added server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 var express = require('express');
-var app = express();
-app.use(express.static(__dirname + "/app"));
-app.listen(process.env.PORT || 3000);
+var appl = express();
+appl.use(express.static(__dirname + "/app"));
+appl.listen(process.env.PORT || 3000);
